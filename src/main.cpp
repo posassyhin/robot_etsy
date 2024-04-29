3b463ac68f8f1a4d0862641c1d3aff3dc40d6c8b
posassyhin <senior.khailoff@gmail.com>
2024-04-29 16:42:29 +0300

    debug main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,40 @@
-﻿#include "Utils.h"  // Подключение созданного заголовочного файла
+﻿#include "Utils.h"
+#include <iostream>
 
-int main() {
-    // Пример использования метода convertVectorToString
-    std::vector<unsigned char> data = {'H', 'e', 'l', 'l', 'o'};
-    std::string resultString = Utils::convertVectorToString(data);
-    std::cout << "Преобразованная строка: " << resultString << std::endl;
+int main(int argc, char* argv[]) {
+    std::string query;
+    int pages = 1;
+    std::vector<std::string> parsed_links;
+    ProxyManager proxy_list("../proxy_list.txt");
 
-    // Пример использования метода get_parsed_links
-    std::string filename = "links.txt";
-    std::vector<std::string> links = Utils::get_parsed_links(filename);
-    std::cout << "Считанные ссылки:" << std::endl;
-    for (const auto& link : links) {
-        std::cout << link << std::endl;
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg == "--help" || arg == "-h") {
+            Utils::display_help();
+            return 0;
+        } else if ((arg == "--query" || arg == "-q") && i + 1 < argc) {
+            query = argv[++i];
+            if ((arg == "--pages" || arg == "-p") && i + 1 < argc) {
+                pages = std::stoi(argv[++i]);
+            }
+            if (Utils::run_search(query, pages)) {
+                std::cout << "Поиск выполнен успешно." << std::endl;
+            } else {
+                std::cerr << "Ошибка при выполнении поиска." << std::endl;
+                return 1;
+            }
+        } else if ((arg == "--repars" || arg == "-r") && i + 1 < argc) {
+            proxy_list.CreateValidProxies();
+            parsed_links = Utils::get_parsed_links(argv[++i]);
+            for (const auto& link : parsed_links) {
+                Utils::save_all_images_by_link(link, proxy_list, true);
+            }
+            return 0;
+        } else {
+            std::cerr << "Unknown or incomplete argument: " << arg << std::endl;
+            Utils::display_help();
+            return 1;
+        }
     }
-
-    // Пример использования метода save_to_file
-    std::string filepath = "output.bin";
-    if (Utils::save_to_file(data, filepath, false)) {
-        std::cout << "Данные успешно сохранены в файл " << filepath << std::endl;
-    } else {
-        std::cout << "Ошибка при сохранении данных." << std::endl;
-    }
-
-    // Пример использования метода display_help
-    Utils::display_help();
-
-    // Пример использования метода run_search
-    std::string query = "example query";
-    int pages = 3;
-    if (Utils::run_search(query, pages)) {
-        std::cout << "Поиск выполнен успешно." << std::endl;
-    } else {
-        std::cout << "Ошибка при выполнении поиска." << std::endl;
-    }
-
     return 0;
 }
